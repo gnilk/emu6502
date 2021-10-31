2e5601d51decfdb4178dc400ccc7d441690fe708
gnilk <kling.fredrik@gmail.com>
2021-10-31 19:34:41 +0100

    Added empty ui skeleton for macOS so it can build

---
+++ src/macOS/ui.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Fredrik Kling on 31/10/2021.
+//
+
+int ui_initialize() {
+    return 0;
+}
+bool ui_beginframe() {
+    return false;
+}
+void ui_endframe() {
+
+}
+void ui_close() {
+
+}
