c11d8d8daa727489c6be89330d38b156281fb780
gnilk <gnilk>
2021-11-07 14:51:26 +0100

    Base VIC 6569 PAL implementation Trying to be some-what cycle correct.

--- src/Memory.h
+++ src/Memory.h
@@ -16,12 +16,18 @@ public:
 
     void CopyTo(uint32_t dstIndex, const void *src, size_t nBytes);
     const uint8_t *RawPtr() { return ram; }
+    uint8_t *PtrAt(uint32_t index) { return &ram[index]; }
     uint8_t ReadU8(uint32_t index);
     uint16_t ReadU16(uint32_t index);
     uint32_t ReadU32(uint32_t index);
     void WriteU8(uint32_t index, uint8_t value);
     void WriteU16(uint32_t index, uint16_t value);
     void WriteU32(uint32_t index, uint32_t value);
+
+    inline uint8_t &operator[](const size_t index) noexcept {
+        return ram[index];
+    }
+
     // TODO: Support debug flags...
 private:
     size_t szRamBuffer;
